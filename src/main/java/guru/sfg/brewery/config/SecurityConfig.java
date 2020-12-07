01c379dc1e26b61ebbd39d823641ed9919af9d88
Muhammad Afaque Khan <mak.khan199@gmail.com>
2020-12-07 15:56:26 +0100

    added security config to get traffic to url without authentication

---
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package guru.sfg.brewery.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // allowing web traffic to go to those specific URLs
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorize->{
+            authorize.antMatchers("/","/webjars/**","/resources/**").permitAll();
+        }).authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+}
