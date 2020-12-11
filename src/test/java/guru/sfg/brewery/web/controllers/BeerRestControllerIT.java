dfb0b6c8711ef1ca6c38c9cbf0c9e7ee584479e4
Muhammad Afaque Khan <mak.khan199@gmail.com>
2020-12-11 14:32:16 +0100

    added test for deleteBeer API with header for api-key and api-secret

--- src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
@@ -3,11 +3,19 @@ package guru.sfg.brewery.web.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @WebMvcTest
 public class BeerRestControllerIT extends BaseIT{
+
+    @Test
+    public void deleteBeer() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-98c4-4ae0-b663-453e8e19c310")
+        .header("Api-Key","spring").header("Api-Secret","guru")).andExpect(status().isOk());
+    }
+
 
     @Test
     public void findBeers() throws Exception {
