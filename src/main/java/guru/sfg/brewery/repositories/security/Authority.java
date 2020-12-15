bd61193983d7b3a75878fdf783e35f8871a8a74f
Muhammad Afaque Khan <mak.khan199@gmail.com>
2020-12-15 14:04:40 +0100

    added Spring Data JPA configuration

---
+++ src/main/java/guru/sfg/brewery/repositories/security/Authority.java
@@ -0,0 +1,6 @@
+package guru.sfg.brewery.repositories.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Authority extends JpaRepository<Authority,Integer> {
+}
