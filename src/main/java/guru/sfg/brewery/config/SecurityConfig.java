473d76bcba6de685a1cc522f7db1f7006bdd5648
Muhammad Afaque Khan <mak.khan199@gmail.com>
2021-01-18 13:28:10 +0100

    added method security using expression based syntax

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -28,7 +28,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     // constructs restHeaderAuthFilter
